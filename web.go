f1bf0342f007479697c9fcb1dff4409beb5d5d3d
Markus Becker <mtb@tibyte.net>
2016-05-13 23:58:40 +0200

    user friendlier

--- web.go
+++ web.go
@@ -7,20 +7,31 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v not implemented", r.URL.Path)
+	fmt.Fprintf(w, "<html><body>%v not implemented", r.URL.Path)
+	fmt.Fprint(w, `<br>Beispiel Links:<br>
+        <a href="/txt/Berlin/">/txt/Berlin/</a>
+        <a href="/view/Berlin/">/view/Berlin/</a><br>
+        <a href="/txt/Kühlunsgborn/">/txt/Kühlunsgborn/</a> 
+        <a href="/view/Kühlunsgborn/">/view/Kühlunsgborn/</a><br>
+        Text: /txt/<i>ort</i>/<br>Balken: /view/<i>ort</i>/<br>CSV: /csv/<i>ort</i>/
+        </body></html>`)
 	fmt.Printf("Requested: %s\n", r.URL.Path)
 }
 
 func txtHandler(w http.ResponseWriter, r *http.Request) {
-	cut := r.URL.Path[5:]
-	req := cut[:strings.Index(cut, "/")]
-	fmt.Fprintf(w, "%s", PrognoseTxt(req, 3))
+	cut := r.URL.Path[len("/txt/"):]
+	if i := strings.Index(cut, "/"); i != -1 {
+		cut = cut[:i]
+	}
+	fmt.Fprintf(w, "%s", PrognoseTxt(cut, 3))
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	cut := r.URL.Path[6:]
-	req := cut[:strings.Index(cut, "/")]
-	SimpleHTML(req, w)
+	cut := r.URL.Path[len("/view/"):]
+	if i := strings.Index(cut, "/"); i != -1 {
+		cut = cut[:i]
+	}
+	SimpleHTML(cut, w)
 }
 
 func webSetup(port *string) {
