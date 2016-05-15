6d646ec96f5d15e856f5e8ccae65a0ca57fbce21
Markus Becker <mtb@tibyte.net>
2016-05-15 23:08:33 +0200

    old index page

--- web.go
+++ web.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<html><body>%v not implemented", r.URL.Path)
-	fmt.Fprint(w, `<br>Beispiel Links:<br>
-        <a href="/txt/Berlin/">/txt/Berlin/</a>
-        <a href="/view/Berlin/">/view/Berlin/</a><br>
-        <a href="/txt/Kühlunsgborn/">/txt/Kühlunsgborn/</a> 
-        <a href="/view/Kühlunsgborn/">/view/Kühlunsgborn/</a><br>
-        Text: /txt/<i>ort</i>/<br>Balken: /view/<i>ort</i>/<br>CSV: /csv/<i>ort</i>/
-        </body></html>`)
-	fmt.Printf("Requested: %s\n", r.URL.Path)
+	switch r.URL.Path[1:] {
+	case "":
+		serveIndex(w)
+	case "bsp/":
+		fmt.Fprint(w, "<html>Beispiele:<br>")
+		// Bad Code starting
+		orte := []string{"Berlin", "Kühlungsborn", "Oslo", "New York"}
+		for _, v := range orte {
+			fmt.Fprintf(w, "<a href=\"/txt/%s/\">txt</a> <a href=\"/view/%s/\">view</a> %s<br>", v, v, v)
+		}
+	}
 }
 
 func txtHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,9 +39,47 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	SimpleHTML(cut, w)
 }
 
+func resourceHandler(w http.ResponseWriter, r *http.Request) {
+	s := r.URL.Path[len("/resources/"):]
+	switch s {
+	case "main.css":
+		styleTemplate.Execute(w, nil)
+	default:
+		r, ok := resources[s]
+		if !ok || r == "" {
+			fmt.Fprintf(w, "404 - not found %s", s)
+			return
+		}
+		io.WriteString(w, r)
+	}
+}
+
 func webSetup(port *string) {
 	http.HandleFunc("/txt/", txtHandler)
 	http.HandleFunc("/view/", viewHandler)
+	// http.Handle("/resources/", http.FileServer(http.Dir("./resources/")))
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/resources/", resourceHandler)
 	http.ListenAndServe(*port, nil)
+}
+
+// Handler Helper
+var indexTemplate, _ = template.ParseFiles("./template/index.html")
+var styleTemplate, _ = template.ParseFiles("./template/main.css")
+
+var resources = map[string]string{
+	"logo.png": load("logo.png"),
+}
+
+func load(res string) string {
+	bt, err := ioutil.ReadFile(fmt.Sprintf("./resources/%s", res))
+	if err != nil {
+		fmt.Printf("trying to load %s, not found", res)
+		return ""
+	}
+	return string(bt)
+}
+
+func serveIndex(w io.Writer) {
+	indexTemplate.Execute(w, nil)
 }
