022b9e70c7ab50373b164a482e56e1a08c270ad3
Markus Becker <mtb@tibyte.net>
2016-05-17 18:46:37 +0200

    better arrow

--- simple_view.go
+++ simple_view.go
@@ -30,7 +30,7 @@ func fillSimpleTemplate(city string, w io.Writer) {
 		[]WeatherValue{
 			WeatherValue{"Temperatur:", template.HTML(fmt.Sprintf("%.1f °C", d.Main.Temp-272.15))},
 			WeatherValue{"Windstärke:", template.HTML(fmt.Sprintf("%.1f km/h", d.Wind.Speed*1.852))},
-			WeatherValue{"Windrichtung:", template.HTML(fmt.Sprintf(`<span style="-ms-transform:rotate(%.2fdeg); -webkit-transform:rotate(%.2fdeg); transform:rotate(%.2fdeg); display:block;position:absolute;right:9em;">&#8613;</span> %.1f Grad`, d.Wind.Deg, d.Wind.Deg, d.Wind.Deg, d.Wind.Deg))},
+			WeatherValue{"Windrichtung:", template.HTML(fmt.Sprintf(`<span class="notonmobile" style="-ms-transform:rotate(%.2fdeg); -webkit-transform:rotate(%.2fdeg); transform:rotate(%.2fdeg); display:block;position:absolute;right:9em;">&#8613;</span> %.1f Grad`, d.Wind.Deg, d.Wind.Deg, d.Wind.Deg, d.Wind.Deg))},
 			WeatherValue{"Luftdruck:", template.HTML(fmt.Sprintf("%.1f hpa", d.Main.Pressure))},
 			WeatherValue{"Luftfeuchtigkeit:", template.HTML(fmt.Sprintf("%d%%", int(d.Main.Humidity)))},
 		},
